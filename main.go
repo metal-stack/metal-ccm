79efcafbddfc6dacf9cd5227083251676cac3c0d
Stefan Majer <stefan.majer@f-i-ts.de>
2020-02-04 11:53:21 +0100

    Remove TODO from controller initialization because k8s issue is closed now with recent versions, set cloudprovider to metal by default

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"math/rand"
 	"os"
@@ -12,21 +11,42 @@ import (
 
 	_ "github.com/metal-pod/metal-ccm/cmd"
 	"github.com/metal-pod/v"
+	"github.com/spf13/pflag"
 )
+
+const providerName = "metal"
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	// Bogus parameter needed until https://github.com/kubernetes/kubernetes/issues/76205
-	// gets resolved.
-	flag.CommandLine.String("cloud-provider-gce-lb-src-cidrs", "", "NOT USED (workaround for https://github.com/kubernetes/kubernetes/issues/76205)")
-
 	command := app.NewCloudControllerManagerCommand()
 
-	// TODO: once we switch everything over to Cobra commands, we can go back to calling
-	// utilflag.InitFlags() (by removing its pflag.Parse() call). For now, we have to set the
-	// normalize func and add the go flag set by hand.
-	// utilflag.InitFlags()
+	// Set static flags for which we know the values.
+	command.Flags().VisitAll(func(fl *pflag.Flag) {
+		var err error
+		switch fl.Name {
+		case "allow-untagged-cloud",
+			// Untagged clouds must be enabled explicitly as they were once marked
+			// deprecated. See
+			// https://github.com/kubernetes/cloud-provider/issues/12 for an ongoing
+			// discussion on whether that is to be changed or not.
+			"authentication-skip-lookup":
+			// Prevent reaching out to an authentication-related ConfigMap that
+			// we do not need, and thus do not intend to create RBAC permissions
+			// for. See also
+			// https://github.com/digitalocean/digitalocean-cloud-controller-manager/issues/217
+			// and https://github.com/kubernetes/cloud-provider/issues/29.
+			err = fl.Value.Set("true")
+		case "cloud-provider":
+			// Specify the name we register our own cloud provider implementation
+			// for.
+			err = fl.Value.Set(providerName)
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to set flag %q: %s\n", fl.Name, err)
+			os.Exit(1)
+		}
+	})
 
 	logs.InitLogs()
 	defer logs.FlushLogs()
