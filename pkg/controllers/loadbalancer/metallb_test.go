9c185d53e6eb57a82d1b45e961f97e0009dc0b20
Stefan Majer <stefan.majer@f-i-ts.de>
2021-09-20 16:39:45 +0200

    Fix linter bug

--- pkg/controllers/loadbalancer/metallb_test.go
+++ pkg/controllers/loadbalancer/metallb_test.go
@@ -237,6 +237,7 @@ func TestMetalLBConfig_CalculateConfig(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			cfg := &MetalLBConfig{
 				logger:           log.New(log.Writer(), "testing", log.LstdFlags),
