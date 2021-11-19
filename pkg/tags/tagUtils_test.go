da18d3d2308c1107b84df9a907caad6ad085b7f0
Stefan Majer <stefan.majer@f-i-ts.de>
2021-11-19 11:33:45 +0100

    add a test for tag parsing

---
+++ pkg/tags/tagUtils_test.go
@@ -0,0 +1,38 @@
+package tags
+
+import "testing"
+
+func TestIsMemberOfCluster(t *testing.T) {
+	tests := []struct {
+		name      string
+		tag       string
+		clusterID string
+		want      bool
+	}{
+		{
+			name:      "valid cluster service tag on ip",
+			tag:       "cluster.metal-stack.io/id/namespace/service=e0ab89d8-c087-4c5a-9e86-7656a2371c24/default/echoserver-ext",
+			clusterID: "e0ab89d8-c087-4c5a-9e86-7656a2371c24",
+			want:      true,
+		},
+		{
+			name:      "valid cluster pod tag on ip",
+			tag:       "cluster.metal-stack.io/id/namespace/pod=e0ab89d8-c087-4c5a-9e86-7656a2371c24/default/echoserver-ext",
+			clusterID: "e0ab89d8-c087-4c5a-9e86-7656a2371c24",
+			want:      true,
+		},
+		{
+			name:      "invalid cluster service tag on ip",
+			tag:       "blubber=e0ab89d8-c087-4c5a-9e86-7656a2371c24/default/echoserver-ext",
+			clusterID: "e0ab89d8-c087-4c5a-9e86-7656a2371c24",
+			want:      false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsMemberOfCluster(tt.tag, tt.clusterID); got != tt.want {
+				t.Errorf("IsMemberOfCluster() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
