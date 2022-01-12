31bb491d663a3116e36c74f3a7958818ab5286ce
Gerrit <info@gerritschwerthelm.de>
2022-01-12 15:34:07 +0100

    Enforce metallb config map reload everytime.
    This prevents the MetalLB controller from reporting a stale config after service type load balancer deletion. More information can be found in the code.

--- pkg/resources/kubernetes/configmap.go
+++ pkg/resources/kubernetes/configmap.go
@@ -2,39 +2,37 @@ package kubernetes
 
 import (
 	"context"
+	"time"
 
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
-// Apply inserts or updates given config map.
+// ApplyConfigMap creates or updates given config map.
 func ApplyConfigMap(client kubernetes.Interface, namespace, name string, configMap map[string]string) error {
-	ctx := context.Background()
-	cmi := client.CoreV1().ConfigMaps(namespace)
-	cm, err := cmi.Get(ctx, name, metav1.GetOptions{})
-	if err == nil {
-		cm.Data = configMap
+	var (
+		ctx = context.Background()
+		cm  = &v1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: namespace,
+				Annotations: map[string]string{
+					// we enforce updating the metallb config every time such that
+					// deleted service ips will be cleaned up from the config regularly.
+					"cluster.metal-stack.io.metal-ccm/last-update-time": time.Now().String(),
+				},
+			},
+			Data: configMap,
+		}
+	)
 
-		_, err = cmi.Update(ctx, cm, metav1.UpdateOptions{})
+	_, err := client.CoreV1().ConfigMaps(namespace).Update(ctx, cm, metav1.UpdateOptions{})
+	if err != nil && errors.IsNotFound(err) {
+		_, err = client.CoreV1().ConfigMaps(namespace).Create(ctx, cm, metav1.CreateOptions{})
 		return err
 	}
 
-	cm = &v1.ConfigMap{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "ConfigMap",
-			APIVersion: "v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:                       name,
-			Namespace:                  namespace,
-			DeletionGracePeriodSeconds: nil,
-			Labels:                     nil,
-			Annotations:                nil,
-		},
-		Data: configMap,
-	}
-
-	_, err = cmi.Create(ctx, cm, metav1.CreateOptions{})
 	return err
 }
