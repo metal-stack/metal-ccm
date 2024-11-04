7cbe3f68340d83aa717ae65523ca47cdadeef534
Stefan Majer <stefan.majer@f-i-ts.de>
2024-11-04 13:01:59 +0100

    More tests

--- pkg/controllers/loadbalancer/config/metallb_config_test.go
+++ pkg/controllers/loadbalancer/config/metallb_config_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/metal-stack/metal-lib/pkg/pointer"
 	"github.com/metal-stack/metal-lib/pkg/tag"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
 )
 
@@ -24,13 +25,120 @@ var (
 
 func TestMetalLBConfig(t *testing.T) {
 	tests := []struct {
-		name    string
-		nws     sets.Set[string]
-		ips     []*models.V1IPResponse
-		nodes   []v1.Node
-		wantErr error
-		want    *metalLBConfig
+		name           string
+		nws            sets.Set[string]
+		ips            []*models.V1IPResponse
+		nodes          []v1.Node
+		wantErrmessage *string
+		want           *metalLBConfig
 	}{
+		{
+			name: "two ips, one with missing networkID acquired, no nodes",
+			nws:  testNetworks,
+			ips: []*models.V1IPResponse{
+				{
+					Ipaddress: pointer.Pointer("84.1.1.1"),
+					Name:      "acquired-before",
+					Networkid: pointer.Pointer("internet"),
+					Projectid: pointer.Pointer("project-a"),
+					Tags: []string{
+						fmt.Sprintf("%s=%s", tag.ClusterID, "this-cluster"),
+					},
+					Type: pointer.Pointer("ephemeral"),
+				},
+				{
+					Ipaddress: pointer.Pointer("84.1.1.2"),
+					Name:      "acquired-before",
+					Networkid: nil,
+					Projectid: pointer.Pointer("project-a"),
+					Tags: []string{
+						fmt.Sprintf("%s=%s", tag.ClusterID, "this-cluster"),
+					},
+					Type: pointer.Pointer("ephemeral"),
+				},
+			},
+			nodes:          []v1.Node{},
+			wantErrmessage: pointer.Pointer("ip has no network id set: 84.1.1.2"),
+			want:           nil,
+		},
+		{
+			name: "two ips, one with missing IP acquired, no nodes",
+			nws:  testNetworks,
+			ips: []*models.V1IPResponse{
+				{
+					Ipaddress: pointer.Pointer("84.1.1.1"),
+					Name:      "acquired-before",
+					Networkid: pointer.Pointer("internet"),
+					Projectid: pointer.Pointer("project-a"),
+					Tags: []string{
+						fmt.Sprintf("%s=%s", tag.ClusterID, "this-cluster"),
+					},
+					Type: pointer.Pointer("ephemeral"),
+				},
+				{
+					Name:      "acquired-before",
+					Networkid: pointer.Pointer("internet"),
+					Projectid: pointer.Pointer("project-a"),
+					Tags: []string{
+						fmt.Sprintf("%s=%s", tag.ClusterID, "this-cluster"),
+					},
+					Type: pointer.Pointer("ephemeral"),
+				},
+			},
+			nodes:          []v1.Node{},
+			wantErrmessage: pointer.Pointer("ip address is not set on ip"),
+			want:           nil,
+		},
+		{
+			name: "one malformed ip acquired, no nodes",
+			nws:  testNetworks,
+			ips: []*models.V1IPResponse{
+				{
+					Ipaddress: pointer.Pointer("84.1.1.1.1"),
+					Name:      "acquired-before",
+					Networkid: pointer.Pointer("internet"),
+					Projectid: pointer.Pointer("project-a"),
+					Tags: []string{
+						fmt.Sprintf("%s=%s", tag.ClusterID, "this-cluster"),
+					},
+					Type: pointer.Pointer("ephemeral"),
+				},
+			},
+			nodes:          []v1.Node{},
+			wantErrmessage: pointer.Pointer("ParseAddr(\"84.1.1.1.1\"): IPv4 address too long"),
+			want:           nil,
+		},
+		{
+			name: "one ipv6 acquired, no nodes",
+			nws:  testNetworks,
+			ips: []*models.V1IPResponse{
+				{
+					Ipaddress: pointer.Pointer("2001::a:b:c"),
+					Name:      "acquired-before",
+					Networkid: pointer.Pointer("internet"),
+					Projectid: pointer.Pointer("project-a"),
+					Tags: []string{
+						fmt.Sprintf("%s=%s", tag.ClusterID, "this-cluster"),
+					},
+					Type: pointer.Pointer("ephemeral"),
+				},
+			},
+			nodes:          []v1.Node{},
+			wantErrmessage: nil,
+			want: &metalLBConfig{
+				Base: &baseConfig{
+					AddressPools: addressPools{
+						"internet-ephemeral": {
+							Name:       "internet-ephemeral",
+							Protocol:   "bgp",
+							AutoAssign: pointer.Pointer(false),
+							CIDRs:      []string{"2001::a:b:c/128"},
+						},
+					},
+					Peers: nil,
+				},
+			},
+		},
 		{
 			name: "one ip acquired, no nodes",
 			nws:  testNetworks,
@@ -46,8 +154,8 @@ func TestMetalLBConfig(t *testing.T) {
 					Type: pointer.Pointer("ephemeral"),
 				},
 			},
-			nodes:   []v1.Node{},
-			wantErr: nil,
+			nodes:          []v1.Node{},
+			wantErrmessage: nil,
 			want: &metalLBConfig{
 				Base: &baseConfig{
 					AddressPools: addressPools{
@@ -62,6 +170,101 @@ func TestMetalLBConfig(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "one ip acquired, one node with malformed ASN label",
+			nws:  testNetworks,
+			ips: []*models.V1IPResponse{
+				{
+					Ipaddress: pointer.Pointer("84.1.1.1"),
+					Name:      "acquired-before",
+					Networkid: pointer.Pointer("internet"),
+					Projectid: pointer.Pointer("project-a"),
+					Tags: []string{
+						fmt.Sprintf("%s=%s", tag.ClusterID, "this-cluster"),
+					},
+					Type: pointer.Pointer("ephemeral"),
+				},
+			},
+			nodes: []v1.Node{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "node-1",
+						Labels: map[string]string{
+							tag.MachineNetworkPrimaryASN: "abc",
+						},
+					},
+				},
+			},
+			wantErrmessage: pointer.Pointer("unable to parse valid integer from asn annotation: strconv.ParseInt: parsing \"abc\": invalid syntax"),
+			want:           nil,
+		},
+		{
+			name: "one ip acquired, one node without ASN label",
+			nws:  testNetworks,
+			ips: []*models.V1IPResponse{
+				{
+					Ipaddress: pointer.Pointer("84.1.1.1"),
+					Name:      "acquired-before",
+					Networkid: pointer.Pointer("internet"),
+					Projectid: pointer.Pointer("project-a"),
+					Tags: []string{
+						fmt.Sprintf("%s=%s", tag.ClusterID, "this-cluster"),
+					},
+					Type: pointer.Pointer("ephemeral"),
+				},
+			},
+			nodes: []v1.Node{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:   "node-1",
+						Labels: nil,
+					},
+				},
+			},
+			wantErrmessage: pointer.Pointer("node \"node-1\" misses label: machine.metal-stack.io/network.primary.asn"),
+			want:           nil,
+		},
+		{
+			name: "one ip acquired, one node with malformed ASN label",
+			nws:  testNetworks,
+			ips: []*models.V1IPResponse{
+				{
+					Ipaddress: pointer.Pointer("84.1.1.1"),
+					Name:      "acquired-before",
+					Networkid: pointer.Pointer("internet"),
+					Projectid: pointer.Pointer("project-a"),
+					Tags: []string{
+						fmt.Sprintf("%s=%s", tag.ClusterID, "this-cluster"),
+					},
+					Type: pointer.Pointer("ephemeral"),
+				},
+			},
+			nodes: []v1.Node{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "node-1",
+						Labels: map[string]string{
+							tag.MachineNetworkPrimaryASN: "42",
+						},
+					},
+				},
+			},
+			wantErrmessage: nil,
+			want: &metalLBConfig{
+				Base: &baseConfig{
+					AddressPools: addressPools{
+						"internet-ephemeral": {
+							Name:       "internet-ephemeral",
+							Protocol:   "bgp",
+							AutoAssign: pointer.Pointer(false),
+							CIDRs:      []string{"84.1.1.1/32"},
+						},
+					},
+					Peers: nil,
+				},
+			},
+		},
+
 		{
 			name: "two ips acquired, no nodes",
 			nws:  testNetworks,
@@ -87,8 +290,8 @@ func TestMetalLBConfig(t *testing.T) {
 					Type: pointer.Pointer("ephemeral"),
 				},
 			},
-			nodes:   []v1.Node{},
-			wantErr: nil,
+			nodes:          []v1.Node{},
+			wantErrmessage: nil,
 			want: &metalLBConfig{
 				Base: &baseConfig{
 					AddressPools: addressPools{
@@ -141,8 +344,8 @@ func TestMetalLBConfig(t *testing.T) {
 					Type: pointer.Pointer("static"),
 				},
 			},
-			nodes:   []v1.Node{},
-			wantErr: nil,
+			nodes:          []v1.Node{},
+			wantErrmessage: nil,
 			want: &metalLBConfig{
 				Base: &baseConfig{
 					AddressPools: addressPools{
@@ -243,8 +446,8 @@ func TestMetalLBConfig(t *testing.T) {
 					Type: pointer.Pointer("static"),
 				},
 			},
-			nodes:   []v1.Node{},
-			wantErr: nil,
+			nodes:          []v1.Node{},
+			wantErrmessage: nil,
 			want: &metalLBConfig{
 				Base: &baseConfig{
 					AddressPools: addressPools{
@@ -307,8 +510,10 @@ func TestMetalLBConfig(t *testing.T) {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			cfg, err := New(LoadBalancerTypeMetalLB, tt.ips, tt.nws, tt.nodes, nil, nil)
-			if diff := cmp.Diff(err, tt.wantErr); diff != "" {
-				t.Errorf("error = %v", diff)
+			if err != nil {
+				if diff := cmp.Diff(err.Error(), *tt.wantErrmessage); diff != "" {
+					t.Errorf("error = %v", diff)
+				}
 				return
 			}
 
